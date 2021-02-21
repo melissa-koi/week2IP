576e644d16e6b474fe723cb9c9f3ba6138535cf3
melissa-koi <melissawangui3@gmail.com>
2021-02-21 22:12:57 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+let resultPar = document.getElementById('output')
+
+
+function getInput(event) {
+    event.preventDefault();
+    let birthdate = document.getElementById('bday').value
+    let myBdate = new Date(birthdate).toDateString()
+    let day = myBdate.slice(0,3);
+
+    let gender= document.querySelector('input[name="gender"]:checked').value;
+    
+    if(gender === "male" && day === "Sun") {
+        console.log("Kwasi")
+    }else if(gender === "male" && day === "Mon") {
+        console.log("Kwadwo")
+    }else if(gender === "male" && day === "Tue"){
+        console.log("Kwabena")
+    }else if(gender === "male" && day == "Wed") {
+        Name="Kwaku"
+    }else if(gender === "male" && day === "Thu") {
+        Name="Yaw"
+    }else if(gender === "male" && day === "Fri") {
+        Name="Kofi"
+    }else if(gender === "male" && day === "Sat") {
+        Name="Kwame"
+    }else if(gender === "female" && day === "Sun") {
+        Name="Akosua"
+    }else if(gender === "female" && day === "Mon") {
+        Name="Adwoa"
+    }else if(gender === "female" && day === "Tue") {
+        Name="Abenaa"
+    }else if(gender === "female" && day === "Wed") {
+        Name="Akua"
+    }else if(gender === "female" && day === "Thu") {
+        Name="Yaa"
+    }else if(gender === "female" && day === "Fri") {
+        Name="Afua"
+    }else if(gender === "female" && day === "Sat") {
+        Name="Ama"
+    }else {
+        Name="Invalid answer";
+    }
+
+    let message = `Your Akan name is ${Name}`
+    resultPar.innerHTML = message;
+}
+
